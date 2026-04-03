2874d7ffd1e08c818485a9023007255bba316ceb
realfaizandev <s.faizanalib@gmail.com>
2026-04-03 16:00:35 +0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import logo from '../assets/react.svg';
+export default function Header() {
+    const [theme, setTheme] = useState(() => {
+        try {
+            return JSON.parse(localStorage.getItem("theme")) || "gTwo";
+        } catch (e) {
+            console.error("Error parsing theme from localStorage:", e);
+            return "gTwo";
+        }
+    });
+    useEffect(() => {
+        localStorage.setItem("theme", JSON.stringify(theme));
+        document.documentElement.removeAttribute("class");
+        document.documentElement.classList.add(theme);
+    }, [theme]);
+    return (
+        <>
+            <header>
+                <div className="logo">
+                    <img src={logo} alt="App Logo" />
+                    <span>TaskApp</span>
+                </div>
+                <div className="themeSelector">
+                    <span className={theme === "light" ? "light activeTheme" : "light"}
+                        onClick={() => setTheme("light")}
+                    ></span>
+                    <span className={theme === "medium" ? "medium activeTheme" : "medium"}
+                        onClick={() => setTheme("medium")}
+                    ></span>
+                    <span className={theme === "dark" ? "dark activeTheme" : "dark"}
+                        onClick={() => setTheme("dark")}
+                    ></span>
+                    <span className={theme === "gOne" ? "gOne activeTheme" : "gOne"}
+                        onClick={() => setTheme("gOne")}
+                    ></span>
+                    <span className={theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}
+                        onClick={() => setTheme("gTwo")}
+                    ></span>
+                    <span className={theme === "gThree" ? "gThree activeTheme" : "gThree"}
+                        onClick={() => setTheme("gThree")}
+                    ></span>
+                </div>
+            </header >
+        </>
+    );
+}
